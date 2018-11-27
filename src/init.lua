a07b3b8763bbe644443d9a3f55e562e19a44e364
Tsvetkov Maksim <max.tsvetkov@yandex.ru>
2018-11-27 23:37:37 +0300

    First commit

---
+++ src/init.lua
@@ -0,0 +1,118 @@
+GLOBAL_CONSTANTS = {
+    SETTINGS = {
+        ssid_settings_name = "ESP8266_CONFIG",
+        filename = "settings.ini",
+        ssid_prefix = "SSID=",
+        password_prefix = "PASSWORD="
+    },
+
+    SETUP_MODE = false,
+
+    FATAL_ERROR = nil,
+        
+    BUTTON_PIN = { 5, 6 },
+
+    OUTPUT_PIN = { 1, 4 },
+
+    BUTTON_BINDING = { [5] = 1, [6] = 4 },
+    
+    has_value = function(tab, val)
+        for index, value in pairs(tab) do
+            if value == val then
+                return true
+            end
+        end
+        return false
+    end
+}
+
+
+local switching_mode_pin = 5
+local settings_file = file.open(GLOBAL_CONSTANTS["SETTINGS"]["filename"], "r")
+
+gpio.mode(switching_mode_pin, gpio.INT, gpio.PULLUP)
+
+print(gpio.read(switching_mode_pin))
+if(gpio.read(switching_mode_pin) == 0 or settings_file == nil) then
+    print("Setup mode")
+    GLOBAL_CONSTANTS["SETUP_MODE"] = true
+    wifi.setmode(wifi.SOFTAP, false)
+    wifi.ap.config({ssid=GLOBAL_CONSTANTS["SETTINGS"]["ssid_settings_name"], auth=wifi.OPEN, save=false})    
+else
+    print("Normal mode")
+    local find_pattern = function(field)
+        return field.."([^\r\n]*)"
+    end
+
+    local _, _, _, ssid = pcall(function() 
+        return settings_file:readline():find(find_pattern(GLOBAL_CONSTANTS["SETTINGS"]["ssid_prefix"])) 
+        end)
+    local _, _, _, password = pcall(function() 
+        return settings_file:readline():find(find_pattern(GLOBAL_CONSTANTS["SETTINGS"]["password_prefix"]))
+        end)
+
+    --print(pcall(settings_file:readline():find, find_pattern("SSID")))
+    --print(password)
+   if ssid ~= nil and password ~=nil then
+   
+        print("Gpio start")
+        local gpio_result, gpio_string_error = pcall(loadfile("gpio.lua"))
+        if not gpio_result then
+            GLOBAL_CONSTANTS["FATAL_ERROR"] = gpio_string_error
+            print("Gpio: "..GLOBAL_CONSTANTS["FATAL_ERROR"])
+        else
+            print("Gpio ok")
+        end        
+
+        print("Wifi start")
+        local wifi_result, wifi_string_error = pcall(loadfile("wifi_station.lua"), ssid, password)
+        print(wifi_result)
+        if not wifi_result then
+            GLOBAL_CONSTANTS["FATAL_ERROR"] = wifi_string_error
+            print("Wifi: "..GLOBAL_CONSTANTS["FATAL_ERROR"])
+        else
+            print("Wifi ok")
+        end
+               
+   else
+        GLOBAL_CONSTANTS["FATAL_ERROR"] = "Settings corrupted";
+        print(GLOBAL_CONSTANTS["FATAL_ERROR"])
+   end
+    
+end
+
+if(settings_file ~= nil) then
+    settings_file:close()
+end
+
+
+srv=net.createServer(net.TCP) 
+if srv then 
+    srv:listen(80,function(conn) 
+        conn:on("receive",function(conn,payload) 
+            --parsing http header
+            local _, _, url = payload:gmatch("[^\r\n]+")():find("GET ([%a%d%p]*) HTTP")
+
+            local response = "HTTP/1.1 200 OK\r\n\r\n"
+
+            if GLOBAL_CONSTANTS["FATAL_ERROR"] ~= nil then
+                response = response..GLOBAL_CONSTANTS["FATAL_ERROR"]        
+            elseif url ~= nil then
+                local success, string_error = pcall(loadfile("route.lua"), url)
+                if not success then
+                    response = response.."Error: "..string_error
+                else
+                    response = response.."Ok"
+                end 
+            else
+                response = response.."Header error"
+            end
+
+            print(response)
+            
+            conn:send(response, function(conn)
+                conn:close()
+                end)
+            end) 
+    end)
+end
---
+++ src/route.lua
@@ -0,0 +1,73 @@
+local url = ...
+
+if url == nil then
+    do return end
+end
+
+if GLOBAL_CONSTANTS == nil then
+    error("Run only from init.lua")
+end
+
+local check_pinout = function(pinout)
+    if GLOBAL_CONSTANTS["has_value"](GLOBAL_CONSTANTS["OUTPUT_PIN"], tonumber(pinout)) == false then
+        error("Pinout: "..pinout.." is not an output pin")
+    end
+end
+
+local check_settings_mode = function()
+    if GLOBAL_CONSTANTS["SETUP_MODE"] then 
+        error("Unavailable because setup mode is enabled")
+    end
+end
+
+local routing_set = {
+    station = function(ssid)
+        return function(password)
+            local fd = file.open(GLOBAL_CONSTANTS["SETTINGS"]["filename"], "w+")
+            fd:writeline(GLOBAL_CONSTANTS["SETTINGS"]["ssid_prefix"]..ssid)
+            fd:writeline(GLOBAL_CONSTANTS["SETTINGS"]["password_prefix"]..password)
+            fd:flush()
+            fd:close()
+            node.restart()
+        end
+    end
+}
+
+local routing_action = {
+    on = function(pinout)
+        check_pinout(pinout)
+        gpio.write(pinout, gpio.HIGH)
+    end,
+
+    off = function(pinout)
+        check_pinout(pinout)
+        gpio.write(pinout, gpio.LOW)
+    end,
+    inversion = function(pinout)
+        check_pinout(pinout)
+        gpio.write(pinout, gpio.read(pinout) == 1 and gpio.LOW or gpio.HIGH)
+    end 
+}
+
+local routing = {
+    action = function(action)
+      check_settings_mode() 
+      return routing_action[action]  
+    end,
+    set = function(set)
+     return routing_set[set]
+    end
+}
+
+local next_part = url:gmatch("[^/]+")
+local route = routing[next_part()] 
+
+while route ~= nil do
+    local t = type(route)
+    if t == "function" then
+      route = route(next_part())   
+    elseif v == "table" then
+      route = route[next_part()]
+    end             
+end
+
