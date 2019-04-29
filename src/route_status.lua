fd3a45373488c6f610585747bb3b10290e5fa5ef
Maksim Tsvetkov <max.tsvetkov@yandex.ru>
2019-04-29 23:20:18 +0300

    fix status for ap mode

--- src/route_status.lua
+++ src/route_status.lua
@@ -17,14 +17,12 @@ result[#result + 1] = "\"uptime\":"..now_time..","
 result[#result + 1] = "\"heap\":"..node.heap()..","
 
 result[#result + 1] = "\"wifi_info\":{"
-	result[#result + 1] = "\"connection_timepoint\":"..(now_time - GLOBAL_CONSTANTS["WIFI_INFO"]["CONNECTION_TIMEPOINT"])..","
+	if GLOBAL_CONSTANTS["WIFI_STATE"] == "as_station" then
+    	result[#result + 1] = "\"connection_timepoint\":"..(now_time - GLOBAL_CONSTANTS["WIFI_INFO"]["CONNECTION_TIMEPOINT"])..","
+		result[#result + 1] = "\"rssi\":"..wifi.sta.getrssi()..","
+	end
 	result[#result + 1] = "\"reconnect_count\" :"..GLOBAL_CONSTANTS["WIFI_INFO"]["RECONNECT_COUNT"]
 	result[#result + 1] = ","
-
-	if GLOBAL_CONSTANTS["WIFI_STATE"] == "as_station" then
-		result[#result + 1] = "\"rssi\":"..wifi.sta.getrssi()..""
-		result[#result + 1] = ","
-	end
 
 	if GLOBAL_CONSTANTS["WIFI_INFO"]["LAST_REASON_RECONNECTION"] ~= nil then
 		result[#result + 1] = "\"last_reason_reconnection\":\""..GLOBAL_CONSTANTS["WIFI_INFO"]["LAST_REASON_RECONNECTION"].."\""
