70db102067065d31e99741b39ccdbee3d60f91bd
Maksim Tsvetkov <max.tsvetkov@yandex.ru>
2019-06-10 14:58:29 +0300

    Added generation of version from current revision when upload files

--- utils/env.cmd
+++ utils/env.cmd
@@ -1,3 +1,5 @@
-@SET PYTHON_PATH="C:\Program Files\Python\Python37\python.exe"
-@SET PORT="COM10"
-@SET SPEED=115200
+@echo OFF
+SET PYTHON_PATH="C:\Program Files\Python\Python37\python.exe"
+SET PORT="COM10"
+SET SPEED=115200
+echo ON
--- utils/upload_files.cmd
+++ utils/upload_files.cmd
@@ -1,5 +1,16 @@
 @call "env.cmd"
-@cd ..\src
+
+@echo OFF
+
+cd ..\src
+
+FOR /f "tokens=* USEBACKQ" %%i IN (`git log --pretty^=format:"%%h" -n 1`) DO SET current_commit=%%i
+echo %current_commit% > version
+
+echo ^-^-^> Current commit: %current_commit%
+
+echo ON
+
 @FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b /o:gn`) DO ( %PYTHON_PATH% ..\utils\nodemcu-uploader.py -b %SPEED% --start_baud %SPEED% -p %PORT% upload "%%F")
 
 pause
